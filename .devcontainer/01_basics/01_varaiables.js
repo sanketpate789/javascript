3c0f00505d1b352b7682560c4f98beb5cdcc24c5
sanketpate789 <95645154+sanketpate789@users.noreply.github.com>
2024-06-02 01:39:36 +0000

    variables

---
+++ .devcontainer/01_basics/01_varaiables.js
@@ -0,0 +1,13 @@
+const AccountID=23314;
+let AccountEmail="sanket@gmail.com";
+var Account="Sanket123"
+City="latur";
+/* when value is not defined js decleares it as undefined */
+let AccountState;
+/*
+dont use var keyword as it creates problems in definig scope so its beeter to use let
+*/
+AccountEmail="Sanketpate@gamil.com";
+Account="Sanket12";
+city="pune";
+console.table([AccountID, AccountEmail, Account, city]);
